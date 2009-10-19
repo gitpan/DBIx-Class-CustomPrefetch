60d096d44d42330bb8eec79006a3a154b351b541
Андрій Костенко <andrey@kostenko.name>
2009-10-19 15:53:04 +0000

    Import of GUGU/DBIx-Class-CustomPrefetch-0.04 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-CustomPrefetch
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         GUGU/DBIx-Class-CustomPrefetch-0.04.tar.gz
    gitpan-cpan-author:       GUGU
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/CustomPrefetch.pm
+++ lib/DBIx/Class/CustomPrefetch.pm
@@ -24,11 +24,11 @@ This module provides other logic for prefetching data to resultsets.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
